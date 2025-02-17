2e6fe43b0c60a51251cc42288aa4953175ce2d4f
PRASHANT KUMAR <kumarprashant6080@gmail.com>
2025-02-17 19:09:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active')
+}
+
+var typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Backend Developer','Data Scientist','AI ML Enthusiast'],
+    typeSpeed: 80,
+    backspeed: 80,
+    backDelay: 1200,
+    loop: true
+  });
+
+
+
